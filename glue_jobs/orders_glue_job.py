38350e53ef49850bb37dff9af9be5c24af751b02
RSreenath <sreenathravidra123@gmail.com>
2026-03-07 00:46:30 +0530

    code has been modified

--- glue_jobs/orders_glue_job.py
+++ glue_jobs/orders_glue_job.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.context import SparkContext
-from pyspark.sql.functions import col, trim, lower, current_timestamp
+from pyspark.sql.functions import col, trim, lower, current_timestamp, year, month, dayofmonth
 from pyspark.sql.types import IntegerType, DoubleType
 from awsglue.context import GlueContext
 from awsglue.job import Job
@@ -30,12 +30,13 @@ df = spark.read.option("header","true").csv(input_path)
 print("Schema:")
 df.printSchema()
 
-print("Sample data:")
-df.show(5)
+rows = df.count()
+print("Total rows:", rows)
 
-print("Total rows:", df.count())
+# -------------------------
+# Transformations
+# -------------------------
 
-# Transform
 df_clean = (
     df.withColumn("order_id", col("order_id").cast(IntegerType()))
       .withColumn("amount", col("amount").cast(DoubleType()))
@@ -45,10 +46,22 @@ df_clean = (
       .withColumn("processed_timestamp", current_timestamp())
 )
 
-print("Writing parquet to:", output_path)
+# -------------------------
+# Partition columns
+# -------------------------
 
-df_clean.write \
+df_partitioned = (
+    df_clean
+    .withColumn("year", year("processed_timestamp"))
+    .withColumn("month", month("processed_timestamp"))
+    .withColumn("day", dayofmonth("processed_timestamp"))
+)
+
+print("Writing partitioned parquet to:", output_path)
+
+df_partitioned.write \
     .mode("append") \
+    .partitionBy("year","month","day") \
     .parquet(output_path)
 
 print("Write completed successfully")
@@ -56,5 +69,3 @@ print("Write completed successfully")
 job.commit()
 
 print("===== JOB COMPLETED =====")
-print("Row count:", df.count())
-df.show(10)
